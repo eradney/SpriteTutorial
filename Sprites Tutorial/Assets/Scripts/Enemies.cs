7a9e670c040c4e51e07a33c1a276a18916c23d09
eradney <eradney@knights.ucf.edu>
2019-07-19 03:49:49 -0400

    Challenge commit

---
+++ Sprites Tutorial/Assets/Scripts/Enemies.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Vector3 pointA;
+    public Vector3 pointB;
+
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+    }
+
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+        if(count >= 8)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Sprites Tutorial/Assets/Scripts/PlayerController.cs
+++ Sprites Tutorial/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    private Animator anim;
+    private int count;
+    private int lives;
+    private bool facingRight = true;
+
     public float speed;
     public float jumpForce;
+    public Text winText;
+    public Text countText;
+    public Text scoreText;
+    public Text loseText;
+    public AudioClip Background;
+    public AudioClip WinMusic;
+    public AudioSource MusicSource;
+
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        SetCountText();
+        MusicSource.clip = Background;
+        MusicSource.Play();
     }
 
     void FixedUpdate()
@@ -19,8 +40,34 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = new Vector2(moveHorizontal, 0);
         rb2d.AddForce(movement * speed);
 
+        if(facingRight == false && moveHorizontal > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveHorizontal < 0)
+        {
+            Flip();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+        else
+        {
+            anim.SetInteger("State", 0);
+        }
+
         if (Input.GetKey("escape"))
             Application.Quit();
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 
     void OnCollisionStay2D(Collision2D collision)
@@ -30,7 +77,53 @@ public class PlayerController : MonoBehaviour
             if(Input.GetKey(KeyCode.UpArrow))
             {
                 rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                anim.SetInteger("State", 2);
             }
+
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetScoreText();
+        }
+        if (count == 4)
+        {
+            transform.position = new Vector2(-31.0f, -24.5f);
+            lives = 3;
+        }
+        if (lives == 0)
+        {
+            gameObject.SetActive(false);
+            loseText.text = "You Lose :(";
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 8 && MusicSource.isPlaying)
+        {
+            winText.text = "You win!";
+            MusicSource.Stop();
+            MusicSource.clip = WinMusic;
+            MusicSource.Play();
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Lives: " + lives.ToString();
     }
 }
